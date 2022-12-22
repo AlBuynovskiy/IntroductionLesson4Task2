02e310b03a5e3a55b5832c2a94e90d2bdb2406c8
Aliaksandr Buinouski <AlBuynovskiy@gmail.com>
2022-12-22 13:42:03 +0300

    Program debugging

--- Program.cs
+++ Program.cs
@@ -2,3 +2,25 @@
 /*
 Напишите программу которая принимает на вход число и выдает сумму цифр.
 */
+
+Console.Write("Введите число N: ");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int numberN)
+{
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+}
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine($"Сумма цифр в числе {numberN} равна {sumNumber}");
